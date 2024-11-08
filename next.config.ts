58ee4d7fd60ac707621f08984da038b0b3ee09da
Mohamed-Ashraf <140234307+MohammedAshrf@users.noreply.github.com>
2024-11-08 09:59:06 +0200

    upload files

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,22 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "links.papareact.com",
+      },
+      {
+        protocol: "https",
+        hostname: "image.clerk.com",
+      },
+      {
+        protocol: "https",
+        hostname: ".windows.net",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
